ab508d4d870d8748ae21c20c8ee92125093b7914
angelb <angelb1954@gmail.com>
2022-12-05 23:05:02 +0100

    actualizacion 05-12-2022

--- pruebas.js
+++ pruebas.js
@@ -1,3 +1,22 @@
 
 
 
+let myGlobal = 10;
+function fun1() {
+  oopsGlobal = 5;
+}
+
+// Only change code above this line
+
+function fun2() {
+  let output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
+fun2();
+fun1();
