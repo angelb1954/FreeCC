5e7b6ba2693d69bc0cf7973c7210e9407035eb85
angelb <angelb1954@gmail.com>
2022-12-09 16:09:26 +0100

    09-12-2022

--- pruebas.js
+++ pruebas.js
@@ -1,22 +1,8 @@
 
+function maxMin (max,min){
 
+return Math.floor(Math.random() * (max - min + 1)) + min;
 
-let myGlobal = 10;
-function fun1() {
-  oopsGlobal = 5;
 }
 
-// Only change code above this line
-
-function fun2() {
-  let output = "";
-  if (typeof myGlobal != "undefined") {
-    output += "myGlobal: " + myGlobal;
-  }
-  if (typeof oopsGlobal != "undefined") {
-    output += " oopsGlobal: " + oopsGlobal;
-  }
-  console.log(output);
-}
-fun2();
-fun1();
+console.log(maxMin(30,15))
